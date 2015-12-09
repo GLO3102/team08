a55ec72bd483a32453a9db56c3f70c8f672fefb3
seb-belzile <seb.belzile@gmail.com>
2015-12-08 21:07:22 -0500

    + autosuggestions

--- js/main.js
+++ js/main.js
@@ -4,7 +4,14 @@ require.config({
         underscore: 'http://ajax.cdnjs.com/ajax/libs/underscore.js/1.8.3/underscore-min',
         backbone: 'http://ajax.cdnjs.com/ajax/libs/backbone.js/1.2.3/backbone-min',
         text: 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text',
+        bloodhound: 'https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/bloodhound.min',
+        typeahead: 'typeahead.bundle',
         menuTemplate: 'text!../../Templates/Menu.html'
+    },
+    shim: {
+        typeahead: {
+            deps: ["jquery"]
+        }
     }
 });
 
